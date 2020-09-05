117130dd118c3e5b6f72820b0d32a7dadb63cd8a
Kevin Degila <kayveendegila@gmail.com>
2020-09-05 01:45:11 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Take A Break
+
+A simple python script which reminds you to take a break from your screens
+
+Usage : 
+
+For 3 sessions of 30 min work and 5 min pause in between : 
+
+```commandline
+python main.py --session 3 --duree 30 --pause 5
+```
---
+++ main.py
@@ -0,0 +1,31 @@
+import webbrowser
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--session", "-s", type=int)
+parser.add_argument("--duree", "-d", help="Duree d'une session en minute", type=int)
+parser.add_argument("--pause", "-p", help="La durée de la pause en minute", type=int)
+args = parser.parse_args()
+print(type(args))
+
+
+def session(duree, pause):
+    """
+    Lance une session de travail de durée minute,
+    ouvre une vidéo dans le navigateur
+    et fais une pause de "pause" minute
+    :param duree:
+    :param pause:
+    :return:
+    """
+    print("Une session a commencé")
+    time.sleep(duree * 60)
+    webbrowser.open("https://www.youtube.com/watch?v=yzg94Iamm_c")
+    print("Pause en cours")
+    time.sleep(pause * 60)
+
+
+for i in range(args.session):
+    session(args.duree, args.pause)
+    print(f"Session {i + 1} terminée, il reste {args.session - i - 1} sessions")
